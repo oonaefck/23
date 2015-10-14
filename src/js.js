0f500efb4d8f8c4775fd2facb0289116193fc723
Ville <reg@villeniemi.com>
2015-10-14 10:49:52 +0300

    Flow control via "IF mousemove happens above half the screen, set the log position under the mouse, ELSE above"

--- src/js.js
+++ src/js.js
@@ -1,5 +1,21 @@
+var winX,winY;
+
+$(function(){ init(); });
+$(window).resize(function(){ init(); })
+
+function init(){
+	winX=$(window).width();
+	winY=$(window).height();
+}
+	
 $(window).mousemove(function(e){
 	$('#log').text('x='+e.pageX+' y='+e.pageY);
 	console.log('x='+e.pageX+' y='+e.pageY);
-	$('#log').css({'top':e.pageY-50,'left':e.pageX});
+	if(e.pageY>winY/2) {
+		var logposY = e.pageY-50;
+	}
+	else {
+		var logposY = e.pageY+50;
+	}
+	$('#log').css({'top':logposY,'left':e.pageX});
 });
